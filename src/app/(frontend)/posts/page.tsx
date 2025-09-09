9d2e869dbdc8917c87894dc21ca894aef77b790e
Simon Morgan <simon.morgan80@gmail.com>
2025-09-09 14:52:49 +0100

    fix: removed turbopack from build

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-import { client } from '@/sanity/lib/client';
+// import { client } from '@/sanity/lib/client';
 import { sanityFetch } from '@/sanity/lib/live';
 import { POSTS_QUERY } from '@/sanity/lib/queries';
 
-const options = { next: { revalidate: 60 } };
+// const options = { next: { revalidate: 60 } };
 
 export default async function Page() {
     // const posts = await client.fetch(POSTS_QUERY, {}, options);
